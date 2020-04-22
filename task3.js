8c907bd96b6555018893159fb5f232304189f5f2
Rotimi Adebiyi <r.s.adebiyi@gmail.com>
2020-04-22 19:33:05 +0100

    New code using switch case

--- task3.js
+++ task3.js
@@ -1,7 +1,41 @@
-function task(number){
-    let output = [];
+// declear a variable to an empty array
+let myArray = [];
 
-    for(let i = 1; i <= number; i++){
-        if(i % 2)
+// Checking if the argument is equal to the conditions
+
+function myFunction(num) {
+  // for loop appending num to x
+  for (x = 1; x <= num; x++) {
+    // the switch statement is checking if the loops is equal to the cases iterate else return the value of x".
+    switch (true) {
+      case (x % 2 == 0 && x % 3 == 0 && x % 5 == 0):
+        myArray.push("yu-gi-oh");
+        break;
+      case (x % 2 == 0 && x % 3 == 0):
+        myArray.push("yu-gi");
+        break;
+      case (x % 2 == 0 && x % 5 == 0):
+        myArray.push("yu-oh");
+        break;
+      case (x % 3 == 0 && x % 5 == 0):
+        myArray.push("gi-oh");
+        break;
+      case (x % 2 == 0):
+        myArray.push('yu');
+        break;
+      case (x % 3 == 0):
+        myArray.push('gi');
+        break;
+      case (x % 5 == 0):
+        myArray.push('oh');
+        break;
+      default:
+        myArray.push(x);
+        break;
     }
+  }
+  return myArray;
 }
+// calling myFunction.
+console.log(myFunction(100))
+console.log(myFunction(18));
